de7c3c500eae49c51b155a1e508f02dec124c090
Parth Roy <parthroy@i2i-info.com>
2022-02-14 19:15:32 +0530

    i2i Header

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -6,6 +6,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleUp } from "@fortawesome/free-solid-svg-icons";
 import "./layout.css";
 import { useLocation } from "react-router-dom";
+import linkedin from "../../assets/image/linkedin-brands.svg";
+import facebook from "../../assets/image/facebook-square-brands.svg";
+import github from "../../assets/image/github-brands.svg";
+import instagram from "../../assets/image/instagram-brands.svg";
+import twitter from "../../assets/image/twitter-square-brands.svg";
+// import linkedin from '../../assets/image/linkedin-brands.svg';
 
 function Layout(props) {
   const [isOpen, setIsopen] = useState(false);
@@ -45,14 +51,52 @@ function Layout(props) {
 
   return (
     <>
+      <div className="i2i-header d-flex justify-content-between">
+        <a href="http://www.i2i-info.com/" className="text-white p-2">
+          Developed by : Idea to Implementation Infotech
+        </a>
+        <div className="follow align-self-center">
+          <a
+            href="https://github.com/i2i-info/React-Admin-Panel.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={github} alt="github" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/i2i-infotech"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedin} alt="linkedin" />
+          </a>
+          <a
+            href="https://www.facebook.com/Idea.to.implementation.infotech"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={facebook} alt="facebook" />
+          </a>
+          <a
+            href="https://twitter.com/i2i_infotech"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={twitter} alt="twitter" />
+          </a>
+          <a
+            href="https://www.instagram.com/i2i_infotech"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={instagram} alt="instagram" />
+          </a>
+        </div>
+      </div>
       <div className="position-relative bg-white d-flex p-0">
         {/* <!-- Spinner Start --> */}
 
-        <Sidebar
-          isOpen={isOpen}
-          ToggleSidebar={ToggleSidebar}
-          
-        ></Sidebar>
+        <Sidebar isOpen={isOpen} ToggleSidebar={ToggleSidebar}></Sidebar>
 
         <div className={`content ${isOpen === true ? "active" : ""}`}>
           <Navbar ToggleSidebar={ToggleSidebar}></Navbar>
